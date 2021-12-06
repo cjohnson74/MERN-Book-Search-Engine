3b23b36a8116f64d021a517c6ae7dc8cf35daad4
Carson Johnson <carson74johnson@gmail.com>
2021-12-06 16:10:57 -0500

    imported gql to mutations

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
@@ -13,6 +15,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook, { err }] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -65,12 +69,13 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      // const response = await saveBook(bookToSave, token);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
+      // const { bookToSave } = useMutation(SAVE_BOOK);
+      // console.log(bookToSave)
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,3 +1,5 @@
+import { gql } from "@apollo/client";
+
 export const ADD_USER = gql`
   mutation addUser($email: String, $username: String, $password: String) {
     addUser(username: $username, email: $email, password: $password) {
