ac94899f544f797d6711a1526206a9329161e5a4
Carson Johnson <carson74johnson@gmail.com>
2021-12-08 00:00:21 -0500

    imported in useMuttation and REMOVE_BOOK mutation

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { GET_ME } from '../utils/queries';
+import { REMOVE_BOOK } from '../utils/mutations';
 
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
